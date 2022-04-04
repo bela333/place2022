9c8b116dd788ceb837dea9ddb441dd4f4a601e66
bela333 <b3kstudio@gmail.com>
2022-04-04 10:45:14 +0200

    Added ability to join images

--- get.py
+++ get.py
@@ -95,10 +95,17 @@ def get_url(ws, tag):
 def get_canvas_configs(ws):
   messages = send_message(ws, {'id': '1', 'type': 'start', 'payload': {'variables': {'input': {'channel': {'teamOwner': 'AFD2022', 'category': 'CONFIG'}}}, 'extensions': {}, 'operationName': 'configuration', 'query': config_query}})
   for message in messages:
+    yield (message['canvasWidth'], message['canvasHeight'])
     for config in message['canvasConfigurations']:
-      yield (config['index'], config['dx'], config['dy'])
+      yield (config['index'], config['dx'], config['dy'], )
     return
 
+def join_box(a, b):
+  mina, maxa = a
+  minb, maxb = b
+  _min = tuple(min(a, b) for (a, b) in zip(mina, minb))
+  _max = tuple(max(a, b) for (a, b) in zip(maxa, maxb))
+  return _min, _max
 
 def get_image_url(token):
     ws = websocket.create_connection("wss://gql-realtime-2.reddit.com/query")
@@ -110,33 +117,67 @@ def get_image_url(token):
     })
     ws.send(auth)
 
-    names = []
+    images = []
 
-    for (i, x, y) in get_canvas_configs(ws):
-      names.append(get_url(ws, i))
+    configs = get_canvas_configs(ws)
+
+    w, h = next(configs)
+
+    bounding_box = None
+
+    for (i, x, y) in configs:
+      images.append((x, y, get_url(ws, i)))
+      _box = ((x, y), (x+w, y+h))
+      if bounding_box is None:
+        bounding_box = _box
+      else:
+        bounding_box = join_box(bounding_box, _box)
+    
 
 
 
     ws.close()
-    return names
+    return images, bounding_box
 
 
 if __name__ == "__main__":
     import os
     import time
     import math
+    import sys
+
+    if "-h" in sys.argv:
+      print("--join: Joins together the different parts of the image (Requires Pillow)")
+      sys.exit()
+
+    should_join = "--join" in sys.argv
 
     output_dir = "frames"
     
     os.makedirs(output_dir, exist_ok=True)
 
     token = get_token()
-    urls = get_image_url(token)
+    images, bounding_box = get_image_url(token)
 
-    for i, url in enumerate(urls):
-      filename = "{}_{}.png".format(math.floor(time.time()), i)
-      path = os.path.join(output_dir, filename)
 
+
+    if should_join:
+      from PIL import Image
+      from io import BytesIO
+      #Assuming min is (0, 0)
+      output_image = Image.new(size=bounding_box[1], mode="RGB")
+
+    for i, (x, y, url) in enumerate(images):
       req = requests.get(url)
-      with open(path, "wb") as f:
-          f.write(req.content)
+      if not should_join:
+        filename = "{}_{}.png".format(math.floor(time.time()), i)
+        path = os.path.join(output_dir, filename)
+        with open(path, "wb") as f:
+            f.write(req.content)
+      else:
+        part = Image.open(BytesIO(req.content))
+        output_image.paste(part, (x, y))
+    if should_join:
+      filename = "{}.png".format(math.floor(time.time()))
+      path = os.path.join(output_dir, filename)
+      output_image.save(path)
